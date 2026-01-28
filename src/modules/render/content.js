311ce05ccc46597b56262496401efb91f8355445
okolbe <olivia_pauline.kolbe@smail.th-koeln.de>
2026-01-28 13:40:32 +0100

    new content

--- src/modules/render/content.js
+++ src/modules/render/content.js
@@ -1,11 +1,7 @@
 export const content = [
-  { file: './content/.DS_Store', lastModified: '2025-12-16T12:15:35Z', fileType: 'null' },
-  { file: './content/Look at this/Screenshot 2026-01-16 at 15.31.46.png', lastModified: '2026-01-21T20:34:20Z', fileType: 'IMAGE' },
-  { file: './content/Look at this/Screenshot 2026-01-19 at 14.50.43.png', lastModified: '2026-01-21T20:34:20Z', fileType: 'IMAGE' },
-  { file: './content/Look at this/text.txt', lastModified: '2026-01-21T20:34:20Z', fileType: 'TXT' },
-  { file: './content/good day/dolphin.gif', lastModified: '2026-01-21T23:56:50Z', fileType: 'IMAGE' },
-  { file: './content/good day/keyboard.gif', lastModified: '2026-01-21T23:56:50Z', fileType: 'IMAGE' },
-  { file: './content/wassup/1-text.txt', lastModified: '2026-01-21T21:18:31Z', fileType: 'TXT' },
-  { file: './content/wassup/driving-heaven.gif', lastModified: '2026-01-21T21:18:31Z', fileType: 'IMAGE' },
-  { file: './content/wassup/face.gif', lastModified: '2026-01-21T21:18:31Z', fileType: 'IMAGE' },
+  { file: './content/.DS_Store', lastModified: '2026-01-22T12:18:35Z', fileType: 'null' },
+  { file: './content/hello/.DS_Store', lastModified: '2026-01-28T13:08:47Z', fileType: 'null' },
+  { file: './content/test1/IMG_9945.JPG', lastModified: '2026-01-28T13:20:44Z', fileType: 'IMAGE' },
+  { file: './content/test1/test.txt', lastModified: '2026-01-28T13:20:44Z', fileType: 'TXT' },
+  { file: './content/test2/IMG_9868.JPG', lastModified: '2026-01-28T13:19:22Z', fileType: 'IMAGE' },
 ];
